83566f26645303354f336406467815ef4f5e8b77
Adamkadaban <adamkadaban@gmail.com>
2024-11-03 01:47:12 -0500

    add more words

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const breedList = ["Calico", "Tabby", "Orange", "Bengal", "Bombay", "Burmese", "Persian", "Ragdoll", "Siamese", "Sphynx", "Russian"];
+const breedList = ["Calico", "Tabby", "Orange", "Bengal", "Bombay", "Burmese", "Persian", "Ragdoll", "Siamese", "Sphynx", "Russian", "British", "American", "Scottish", "Norwegian", "Turkish", "Himalayan", "Ginger"];
 
-const relatedWords = ["Chicken", "Tenders", "Jack", "Back", "Hack", "Red", "Blue", "Orange", "Green"];
+const relatedWords = ["Chicken", "Tenders", "Jack", "Back", "Hack", "Red", "Blue", "Orange", "Green", "Gator", "Violet", "Swamp", "Albert", "Alberta", "Chmop", "Offensive", "Defensive", "Fries"];
 
-let locations = ["Gainesville", "Orlando", "Roschester", "Charles", "Jacksonville", "Miami"];
+let locations = ["Gainesville", "Orlando", "Roschester", "Charles", "Jacksonville", "Miami", "Tampa", "Charlotte", "Dallas", "Tallahassee", "Ocala", "Sarasota", "Naples", "Melbourne", "Reitz", "Marston", "Turlington", "Broward", "Smathers", "Shands", "Norman", "Anderson", "Malachowsky", "Newell", "Weil","Carleton", "Little", "Hume", "Rawlings"];
 
 const numPasswords = 10;
 
@@ -17,7 +17,7 @@ function generatePasswords() {
     const [breed1, breed2] = pickRandomElements(breedList, 2);
     const relatedWord = pickRandomElement(relatedWords);
     const location = pickRandomElement(locations);
-    const password = `${breed1}-${relatedWord}-${location}-${padNumber(getRandomInt(0, 99))}`;
+    const password = `${breed1}-${relatedWord}-${location}-${padNumber(getRandomInt(0,99))}`;
     const passwordElement = document.createElement("div");
     passwordElement.onclick = (evt) => {
       copyToClipboard(password);
@@ -49,12 +49,23 @@ function padNumber(num) {
   return num.toString().padStart(2, '0');
 }
 
+function showCopyNotification() {
+      const notification = document.getElementById("copyNotification");
+      notification.classList.add("show");
+
+      // Hide the notification after 2 seconds
+      setTimeout(() => {
+          notification.classList.remove("show");
+      }, 800);
+  }
+
 function copyToClipboard(str) {
   const el = document.createElement('textarea');
   el.value = str;
   document.body.appendChild(el);
   el.select();
   document.execCommand('copy');
+  showCopyNotification();
   document.body.removeChild(el);
 }
 
